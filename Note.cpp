4feb3c118afafd2b1d51310fde5ebd1d2d0f6dc2
Todd Bensmiller <toddbensmiller@my.unt.edu>
2019-10-25 15:31:10 -0500

    updated Note

--- Note.cpp
+++ Note.cpp
@@ -1,27 +1,12 @@
 #include "MidiEvent.h"
-#include <cmath>
-class Note
+#include "Note.h"
+Note::Note(smf::MidiEvent event, int ticksPerQuarterNote, int tempoinput, int tickDuration)
 {
-	public:
-		double frequency;
-		double beatDuration;
-		double startBeat;
-		int tempo;
-		int pianoKey;
-		int velocity;
-		Note(smf::MidiEvent event, int ticksPerQuarterNote, int tempoinput)
-		{
-			ev = event;
-			tempo = tempoinput;
-			pianoKey = event[1];
-			velocity = event[2];
-			frequency = pow(2, (double)(pianoKey-69)/12) * 440;
-			startBeat = event.tick / ticksPerQuarterNote;
-			beatDuration = event.getTickDuration() / ticksPerQuarterNote;
-		}
-		
-	private:
-		smf::MidiEvent ev;
-};
-
-int main(){ return 0;}
+	ev = event;
+	tempo = tempoinput;
+	pianoKey = event[1];
+	velocity = event[2];
+	frequency = pow(2, (double)(pianoKey-69)/12) * 440;
+	startBeat = (double)event.tick / ticksPerQuarterNote;
+	beatDuration = (double)tickDuration / ticksPerQuarterNote;
+}
